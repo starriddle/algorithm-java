1d62ef2561065f7913958c21237da97178663676
starriddle <chen.86@outlook.com>
2022-01-25 12:19:07 +0800

    add module leetcode for leetcode-cn.com

---
+++ leetcode/src/leetcode/editor/cn/package-info.java
@@ -0,0 +1,11 @@
+/**
+ * leetcode-cn.com 做题
+ *
+ * 使用 IDEA 插件 LeetCode Editor，github: https://github.com/shuzijun/leetcode-editor
+ *
+ * 自定义模板，类名：Q${question.questionId}$!velocityTool.camelCaseName(${question.titleSlug})
+ *
+ * @author CYL
+ * @since 2022-01-25
+ */
+package leetcode.editor.cn;
